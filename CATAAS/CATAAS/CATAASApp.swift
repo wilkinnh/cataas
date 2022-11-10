6802e72e2e4ef50f0349c0bfe362695ec9a923a5
nate <wilkinnh@gmail.com>
2022-11-10 13:20:05 -0500

    initial project creation

---
+++ CATAAS/CATAAS/CATAASApp.swift
@@ -0,0 +1,17 @@
+//
+//  CATAASApp.swift
+//  CATAAS
+//
+//  Created by nate on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CATAASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
